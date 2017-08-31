b1e2c7503f431c1120031e780e622e34394f7d80
prnp <prnp@cisco.com>
2017-08-31 13:50:35 +0530

    committing blog.js

--- src/main/webapp/blog.js
+++ src/main/webapp/blog.js
@@ -59,7 +59,7 @@ function main(){
 	
 	 addPost =function(){
 		console.log("in add post");
-		var url = "blog";
+		var url = "Blogit/blog";
 		var title = $("#title").val();
 		var body =  $("#body").val();
 		var category = $('#category option:selected').val();
@@ -101,7 +101,7 @@ function main(){
 		});
 	}
 
-	var url = "blog/getAll";
+	var url = "Blogit/blog/getAll?pageno=1&pagesize=5";
 	$.getJSON(url, function(data) {
 		console.log(data);
 		

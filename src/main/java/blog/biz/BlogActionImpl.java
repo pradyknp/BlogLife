fc47f159a711c2ae775b9f3d5c8f5d3ca6cc756c
prnp <prnp@cisco.com>
2017-08-31 15:46:37 +0530

    committing the code for automation cloud pipeline

--- src/main/java/blog/biz/BlogActionImpl.java
+++ src/main/java/blog/biz/BlogActionImpl.java
@@ -30,7 +30,7 @@ public class BlogActionImpl implements BlogAction {
 	// IBlogViewDAO dao = new InMemoryBlogDAO();
 	// IBlogViewDAO dao = new MongoDAOImpl();
 
-	MongoClient mongoClient = new MongoClient("localhost"); // 27017
+	MongoClient mongoClient = new MongoClient("172.31.45.12"); // 27017
 	Morphia morphia = new Morphia();
 	String databaseName = "blogview";
 	Datastore datastore = morphia.createDatastore(mongoClient, databaseName);

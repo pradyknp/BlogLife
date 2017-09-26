bb4a078ea17cafd4c46a9b59aeb5d58671186b42
prnp <prnp@cisco.com>
2017-09-26 22:39:14 +0530

    committing the Blog UI Signup changes.

--- bloglife/src/components/usercomponent/Signup.jsx
+++ bloglife/src/components/usercomponent/Signup.jsx
@@ -4,12 +4,20 @@ class Signup extends Component {
     constructor(props){
         super(props);
         this.state={
-            url:window.location.origin+"/BlogLife/Blogit"
+            url:window.location.origin+"/BlogLife/Blogit",
+            signedUp:false
         }
         this.createUser=this.createUser.bind(this);
+        this.goBackToSignUp = this.goBackToSignUp.bind(this);
     }
 
+    goBackToSignUp(){
+        this.setState({
+            signedUp:false
+        });
+    }
     createUser (){
+        var local = this;
         var username= document.getElementById("username").value;
         var mailid = document.getElementById("mailid").value;
         var pwd = document.getElementById("password").value;
@@ -57,38 +65,63 @@ class Signup extends Component {
             body:JSON.stringify(data)
         }).then(function(response) {
             console.log(response);
+
+            if(response.status == 500)
+            {
+                alert("The user already exists");
+                local.setState({
+                    signedUp:false
+                });
+                return;
+            }
+
             return response.json();
+
         }).then(function(data) {
             console.log("success");
-            window.location = "/BlogLife/Login";
+            local.setState({
+                signedUp:true
+            });
+            // window.location = "/BlogLife/Login";
         }).catch(function(err) {
             console.log(err);
         });
     }
 
     render() {
-        return (
-             <div style={{'maxHeight':'900px','minHeight':'675px'}}>
-                           <h2 style={{'margin-left':'45%','width':'10%'}}>Sign Up</h2>
+        if(!this.state.signedUp) {
+            return (
+                <div style={{'maxHeight': '900px', 'minHeight': '675px'}}>
+                    <h2 style={{'margin-left': '45%', 'width': '10%'}}>Sign Up</h2>
 
-                             <div className="imgcontainer">
-                               <img src="img_avatar2.png" alt="Avatar" className="avatar"></img>
-                             </div>
+                    <div className="imgcontainer">
+                        <img src="img_avatar2.png" alt="Avatar" className="avatar"></img>
+                    </div>
 
-                             <div className="container">
-                             <label><b>Username</b></label>
-                              <input type="text" id="username" placeholder="Username"/>
-                              <label><b>Mail ID</b></label>
-                              <input type="email" id="mailid" placeholder="Mail ID"/>
-                              <label><b>Password</b></label>
-                              <input type="password" id="password" placeholder="Password"/>
-                              <label><b>Confirm Password</b></label>
-                              <input type="password" id="confirmpassword" placeholder="Password"/>
-                              <button type="submit" onClick={this.createUser}>Create</button>
-                            </div>
+                    <div className="container">
+                        <label><b>Username</b></label>
+                        <input type="text" id="username" placeholder="Username"/>
+                        <label><b>Mail ID</b></label>
+                        <input type="email" id="mailid" placeholder="Mail ID"/>
+                        <label><b>Password</b></label>
+                        <input type="password" id="password" placeholder="Password"/>
+                        <label><b>Confirm Password</b></label>
+                        <input type="password" id="confirmpassword" placeholder="Password"/>
+                        <button type="submit" onClick={this.createUser}>Create</button>
+                    </div>
 
-         </div>
-        );
+                </div>
+            );
+        }
+        else{
+            return(
+                <div>
+                    <div>
+                        <p className="succBlogPost">Welcome !!! You have successfully signed up. Hope you enjoy the stay with us, visit the Login page to login </p></div>
+                    <button className="goBack" onClick={this.goBackToSignUp}>GO Back</button>
+                </div>
+            )
+        }
     }
 }
 
